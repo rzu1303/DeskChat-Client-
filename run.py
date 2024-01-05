069a43c6a8deb985b1165b9f5c66d8e1e7f5ea29
Shah-imran <shahimrans64@gmail.com>
2024-01-05 19:19:38 +0600

    daily commit

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import sys
 import os
 import logging
 import datetime
+import argparse
 
 from desk_chat_client.services import setup, start_client
 
@@ -9,6 +10,7 @@ from desk_chat_client.services import setup, start_client
 logger = logging.getLogger(__name__)
 
 def main():
+    parser = argparse.ArgumentParser(description='Update inventory in acenda.')
     setup()
 
     start_client()
